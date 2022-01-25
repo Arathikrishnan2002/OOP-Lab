e63bb7779593c4008480cf29099d2653f133aa68
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-01-25 12:57:58 +0530

    Update stringpalindrome.java

--- Cycle 2/stringpalindrome.java
+++ Cycle 2/stringpalindrome.java
@@ -1,30 +1,25 @@
 import java.util.*;
-
 class stringpalindrome
 {
-    public static void main(String args [])
-     {  
-        int i;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string");
-        String Str = sc.nextLine();
-        int flag=0;
-        int len = Str.length();
-        for(i=0;i<len/2;i++)
-        {
-            if(Str.charAt(i)!= Str.charAt(len-i-1))
-            {
-                flag=1;
-                break;
-            }          
-        }
-        if(flag==0)
-        {
-           System.out.println("The given string is pallindrome");
-        }
-        else
-        {
-           System.out.println("The given string is not pallindrome");
-        }
-     }
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int i,flag=0;
+		System.out.println("Enter length of a string");
+		int n=sc.nextInt();
+		System.out.println("Enter a string");
+		char s[]=new char[50];
+		for(i=0;i<n;i++)
+			s[i]=sc.next().charAt(0);
+		for(i=0;i<n/2;i++,n--)
+			if(s[i]==s[n-1])
+			{
+				flag=1;
+				break;
+			}
+		if(flag==1)
+			System.out.println("The string is a palindrome");
+		else
+			System.out.println("The string is not a palindrome");
+	}
 }
