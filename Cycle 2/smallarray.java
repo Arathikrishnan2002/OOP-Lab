d54798c4c07c49acd38ab1c459381998d1ca01da
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-01-25 12:06:47 +0530

    Create smallarray.java

---
+++ Cycle 2/smallarray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class smallarray
+{
+    public static void main(String args [])
+     {
+         
+         int i,s;
+         Scanner obj = new Scanner(System.in);
+         System.out.println("Enter the number of elements of array");
+         int n = obj.nextInt();
+         int a[] = new int[20];
+         System.out.println("Enter the elements of array");
+         for(i=0;i<n;i++)
+          {
+              a[i] = obj.nextInt();
+          }
+        
+         s = a[0]; 
+         for(i=1;i<n;i++)
+         { 
+              
+              if (a[i] < s)
+               {
+                   s = a[i];
+               }
+        }
+     
+         System.out.println("Small = "+s);
+     }
+    
+}
