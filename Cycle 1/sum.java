d1e16f574c37e4e2f5bd7cb98771a29ce767a2f7
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-01-25 11:36:49 +0530

    Create sum.java

---
+++ Cycle 1/sum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Sum
+
+{
+
+public static void main(String args[])
+
+{
+
+int x, y, z;
+
+System.out.println("Enter two numbers");
+
+Scanner obj = new Scanner(System.in);
+
+x = obj.nextInt();
+
+y = obj.nextInt();
+
+z = x + y;
+
+System.out.println("Sum of the numbers = " + z);
+
+}
+
+}
+
