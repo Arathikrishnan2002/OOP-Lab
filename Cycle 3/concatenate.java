deebb5277942b38cffb81a5faa076ea0b66c173a
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-01-26 09:53:39 +0530

    Update and rename concate.java to concatenate.java

--- Cycle 3/concatenate.java
+++ Cycle 3/concatenate.java
@@ -1,17 +1,17 @@
-class concate
+class concatenate
 { 
   public static void main(String args[])
    {
-   concate Obj=new concate();
+   concatenate Obj=new concatenate();
    Obj.consum("3"+"8");
    Obj.consum("Good"+" "+"Morning");
    }
   void consum(int x)
   {
-   System.out.println("Concated number:" +x);
+   System.out.println("Concatenated number:" +x);
   }
   void consum(String x)
    {
-    System.out.println("Concated String=" +x);
+    System.out.println("Concatenated String=" +x);
    }
 }
