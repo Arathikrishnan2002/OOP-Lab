42c29d0870151d00f20abe51c872df872746fe94
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-01-26 09:50:56 +0530

    Update perimeter.java

--- Cycle 3/perimeter.java
+++ Cycle 3/perimeter.java
@@ -1,19 +1,27 @@
 import java.util.*;
-class perimeter
+public class perimeter
 {
     public static void main(String args[])
     {
-        perimeter obj=new perimeter(5);
-        perimeter  obj1=new  perimeter(5,4);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of circle :");
+        int a = sc.nextInt();
+        System.out.println("Enter the length of rectangle :");
+        int b = sc.nextInt();
+        System.out.println("Enter the breadth of rectangle :");
+        int c = sc.nextInt();
+        perimeter obj=new perimeter(a);
+        perimeter obj1=new perimeter(b,c);
+        sc.close();
     }
-    perimeter(int r)
+    perimeter(int a)
     {
-        System.out.println("Perimeter of circle is:"+(3.14*2*r));
+        System.out.println("Perimeter of circle is:"+(3.14*2*a));
     }
-    perimeter(int a,int b)
+    perimeter(int n,int m)
     {
-        int p=a+b;
+        int p=n+m;
         int t=2*p;
-        System.out.println("Perimeter  of rectangle is:"+t);
+        System.out.println("Perimeter  of Rectangle is:"+t);
     }
 } 
