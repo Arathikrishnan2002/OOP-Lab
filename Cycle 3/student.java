a6fb9bcf86986f07d603836b90bba8c0b764c8b7
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-01-26 09:06:44 +0530

    Update student.java

--- Cycle 3/student.java
+++ Cycle 3/student.java
@@ -2,46 +2,47 @@ import java.util.*;
 
 class student
 {
-    public static void main(String args[])
-    {
-        Scanner obj = new Scanner(System.in);
-        System.out.print("Enter the name of the student: ");
-        String s = obj.nextLine();
-        System.out.print("Enter the class of the student: ");
-        int c = obj.nextInt();
-        System.out.print("Enter the roll number of the student: ");
-        int r = obj.nextInt();
-        System.out.print("Enter the mark of 1st subject: ");
-        int m1 = obj.nextInt();
-        System.out.print("Enter the mark of 2nd subject: ");
-        int m2 = obj.nextInt();
-        System.out.print("Enter the mark of 3rd subject: ");
-        int m3 = obj.nextInt();
-        System.out.print("Enter the mark of 4th subject: ");
-        int m4 = obj.nextInt();
-        System.out.print("Enter the mark of 5th subject: ");
-        int m5 = obj.nextInt();
-        
-        int t = m1+m2+m3+m4+m5;
-        double p = t / 500.00 * 100.00;
-        String g;
-        if(p>=90)
-            g = "A+";
-        else if(p>=80)
-            g = "A";
-        else if(p>=70)
-            g = "B+";
-        else if(p>=60)
-            g = "B";
-        else if(p>=50)
-            g = "C";
-        else if(p>=40)
-            g = "D";
-        else
-            g = "E";
-            
-        System.out.println("Total Mark is "+t);    
-        System.out.println("Total Percentage is "+p);    
-        System.out.println("Grade is "+g);
+    int c,r,m1,m2,m3,m4,m5;
+    String n;
+    public static void main(String args[]) {
+       student obj =  new student();
+       obj.read();
+       obj.display();
     }
+
+
+    void read() {
+        Scanner ob1 = new Scanner(System.in);
+        System.out.print("Enter name of student: ");
+        n = ob1.nextLine();
+        System.out.print("Enter class of student: ");
+        c = ob1.nextInt();
+        System.out.print("Enter roll no of student: ");
+        r = ob1.nextInt();
+        System.out.print("Enter marks in 1st subject: ");
+        m1 = ob1.nextInt();
+        System.out.print("Enter marks in 2nd subject: ");
+        m2 = ob1.nextInt();
+        System.out.print("Enter marks in 3rd subject: ");
+        m3 = ob1.nextInt();
+        System.out.print("Enter marks in 4th subject: ");
+        m4 = ob1.nextInt();
+        System.out.print("Enter marks in 5th subject: ");
+        m5 = ob1.nextInt();
+        ob1.close();
+    }
+            
+    void display() {
+        System.out.println("\nDisplaying the student details\n");
+        System.out.println("Name of the Student is " + n);
+        System.out.println("Class of the student is "+ c);
+        System.out.println("Roll no of the Student is " + r);
+        System.out.println("Marks on 1st subject is  " + m1);
+        System.out.println("Marks on 2nd subject is  " + m2);
+        System.out.println("Marks on 3rd subject is  " + m3);
+        System.out.println("Marks on 4th subject is  " + m4);
+        System.out.println("Marks on 5th subject is  " + m5);
+        
+    }
+    
 }
