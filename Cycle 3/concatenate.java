54ef908c775957085d1c1416bab66b17ae2d6903
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-01-31 08:34:49 +0530

    Update concatenate.java

--- Cycle 3/concatenate.java
+++ Cycle 3/concatenate.java
@@ -1,18 +1,27 @@
 import java.util.*;
-class concatenate
-{ 
-  public static void main(String args[])
-   {
-   concatenate Obj=new concatenate();
-   Obj.consum("3"+"8");
-   Obj.consum("Good"+" "+"Morning");
-   }
-  void consum(int x)
-  {
-   System.out.println("Concatenated number:" +x);
-  }
-  void consum(String x)
-   {
-    System.out.println("Concatenated String=" +x);
-   }
+public class concatenate
+{
+    public static void main(String args[])
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter 2 strings to be concatenated :");
+        String c = obj.nextLine();
+        String d = obj.nextLine();
+        
+        System.out.print("Enter 2 numbers to be concatenated :");
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        
+        concatenate sc = new  concatenate();
+        sc.consum(a,b);
+        sc.consum(c,d);
+    }
+    void consum(int a,int b)
+       { 
+          System.out.println("Concatenated numbers = " +a+b);
+       }   
+    void consum(String a,String b)  
+       {
+           System.out.println("Concatenated strings = "+a+b);
+       }
 }
